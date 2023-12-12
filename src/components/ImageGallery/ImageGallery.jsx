8299e1dfa22c77d3505728412f9ada1d2e0b346e
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2023-12-12 20:57:47 +0100

    дз3

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,127 @@
+import { searchImages } from 'components/searchImages';
+import { Component } from 'react';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import Notiflix from 'notiflix';
+import { Audio } from 'react-loader-spinner';
+import { Button } from 'components/Button/Button';
+import { Modal } from 'components/Modal/Modal';
+import css from './ImageGallery.module.css';
+
+let innerHeight = window.innerHeight;
+
+export class ImageGallery extends Component {
+  state = {
+    photo: null,
+    status: 'idle',
+    page: 1,
+    totalPage: 0,
+    largeImageURL: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevState)
+    if (
+      prevProps.searchWord !== this.props.searchWord ||
+      prevState.page !== this.state.page
+    ) {
+      this.setState({ status: 'pending' });
+      if (prevProps.searchWord !== this.props.searchWord) {
+        this.setState({page: 1});
+      }
+      searchImages(this.props.searchWord, this.state.page)
+        .then(response => response.json())
+        .then(photo => {
+          if (Object.keys(photo.hits).length === 0) {
+            Notiflix.Notify.failure('Sorry, we found nothing at your request');
+            this.setState({ status: 'rejected' });
+            return;
+          }
+          // if (this.state.page > 1) {
+          //   this.scroll();
+          // }
+          if (this.state.page > 1) {
+            this.setState(prev => ({
+              photo: [...prev.photo, ...photo.hits],
+              status: 'resolved',
+            }), () => {
+              if (this.state.page > 1) {
+                this.scroll();
+              }});
+            console.log(photo);
+          } else {
+            this.setState({
+              photo: photo.hits,
+              totalPage: photo.totalHits,
+              status: 'resolved',
+            });
+            console.log(photo);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          Notiflix.Notify.failure('Sorry, something went wrong');
+          this.setState({ status: 'rejected' });
+        });
+    }
+  }
+
+  handleSubmit = () => {
+    this.setState({ page: this.state.page + 1 });
+    // this.scroll();
+  };
+
+  openModal = (e) => {
+    this.setState((prev) =>({largeImageURL: e.target.dataset.source}))
+  }
+
+  closeModal = () => {
+    this.setState((prev) =>({largeImageURL: ''}))
+  }
+
+  scroll = () => {
+    innerHeight = innerHeight + window.innerHeight
+    window.scrollBy({
+      top: innerHeight,
+      behavior: "smooth",
+    });
+  }
+  
+
+
+  render() {
+    const { photo, status, page, totalPage, largeImageURL } = this.state;
+    if (status === 'pending') {
+      return (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+          wrapperStyle
+          wrapperClass
+        />
+      );
+    } else if (status === 'resolved') {
+      return (
+        <div>
+          <ul className={css.gallery} onClick={this.openModal}>
+            {photo.map(el => (
+              <ImageGalleryItem key={el.id} imageS={el} />
+            ))}
+          </ul>
+          {largeImageURL && <Modal largeImageURL={largeImageURL} closeModal={this.closeModal}/>}
+          {page*12 <= totalPage && <Button handleSubmit={this.handleSubmit} />}
+        </div>
+      );
+    }
+    // return (
+    //   <ul className="gallery">
+    //     {photo &&
+    //       photo.map(el => (
+    //         <ImageGalleryItem key={el.id} imageS={el.webformatURL} />
+    //       ))}
+    //   </ul>
+    // );
+  }
+}
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import css from './Modal.module.css'
+
+export class Modal extends Component {
+  state = {};
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.onKeydownEsc);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.onKeydownEsc);
+  }
+
+  onKeydownEsc = e => {
+    if (e.code === 'Escape') this.props.closeModal();
+  };
+
+  render() {
+    const { largeImageURL, closeModal } = this.props;
+    
+
+    
+    return (
+        <div className={css.Overlay} onClick={closeModal}>
+            <div className="Modal">
+                <img src={largeImageURL} alt="" width="800" />
+            </div>
+        </div>
+    );
+  }
+}
